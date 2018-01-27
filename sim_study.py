1a2de5457377922143030e4659b373fd35b02840
Hongjian <hongjian-wang@outlook.com>
2018-01-27 14:46:09 -0500

    loose convergence condition f_sd from 0.3 to 0.8
    f_sd = 0.3 might be too tight, because one MCMC runs over 2 million
    iterations and still did not converge.
    
    > Iteration 2118937: 7.64812899888 --> 7.84806586109 in 1 steps

--- sim_study.py
+++ sim_study.py
@@ -10,10 +10,10 @@ versions = ["v" + str( x +1) for x in range(n_sim)]
 for v in versions:
     MCMC_softmax_proposal('house-price-softmax-sampler-{}'.format(v),
                           targetName="train_average_house_price",
-                          lmbda=0.01, f_sd=0.3, Tt=0.1)
+                          lmbda=0.01, f_sd=0.8, Tt=0.1)
     naive_MCMC('house-price-naive-sampler-{}'.format(v),
                targetName="train_average_house_price",
-               lmbda=0.01, f_sd=0.3, Tt=0.1)
+               lmbda=0.01, f_sd=0.8, Tt=0.1)
     q_learning('house-price-q-learning-sampler-{}'.format(v),
                targetName='train_average_house_price',
-               lmbda=0.01, f_sd=0.3, Tt=0.1)
+               lmbda=0.01, f_sd=0.8, Tt=0.1)
